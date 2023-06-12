dcc0b3cd9e6415824311de1dee8169a324752a83
Kenya Chan <kenya.chan@gmail.com>
2023-06-13 08:24:30 +1000

    Add ship factory

---
+++ src/ship.js
@@ -0,0 +1,33 @@
+const TYPE = ['Carrier', 'Battleship', 'Submarine', 'Cruiser', 'Destroyer']; 
+const LENGTHS = [5, 4, 3, 3, 2];
+
+function ship(type) {
+	let _type = type;
+	let _hits = 0;
+	let _length = LENGTHS[TYPE.indexOf(type)];
+
+	return {
+		get length() {
+			return _length;
+		},
+
+		get type() {
+			return _type;
+		},
+
+		hits() {
+			return _hits;
+		},
+
+		isSunk() {
+			return _hits < _length ? false : true;
+		},
+
+		hit() {
+			return _hits <= _length ? _hits += 1 : _hits;
+		}
+
+	}
+}
+
+module.exports = ship;
