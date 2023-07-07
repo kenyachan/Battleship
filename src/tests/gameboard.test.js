6153d854b1637f55880ce2cea469a0d5d21eb356
Kenya Chan <kenya.chan@gmail.com>
2023-07-08 09:54:56 +1000

    Refactor

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,30 +1,8 @@
 const Gameboard = require('../gameboard');
 const Coordinates = require('../coordinates');
+const Ship = require('../ship');
 //jest.mock('../coordinates');
-
-/*
- * Game board should know where all the ships are
- * Game board should keep track of where all the missed shots are
- * Game board should keep track of where all the hit shots are
- * Game board should be able to report whether or not all ships have been sunk
- * Game board should be able to report whether a specific ship has been sunk?
- */
-
-/*
- * Game board queries
- * ------------------
- * getShips() - returns array of ships with coordinates of ship
- * getReceivedAttacks() - returns an array coordinates for all attacks
- *
- */
-
-/*
- * Game board commands
- * -------------------
- *  receiveAttack(coordinates)
- *  placeShip(ship, coordinates, direction)
- *
- */
+//jest.mock('../ship');
 
 let board;
 
@@ -32,40 +10,85 @@ beforeEach(() => {
 	board = new Gameboard(Coordinates);
 });
 
-describe('Game board initialisation', () => {
-	test('Create a new game board with a grid of size 10x10', () => {
-		expect(board instanceof Gameboard).toEqual(true);
-		expect(board.getGrid().length).toEqual(100);
+describe('Ship placement', () => {
+	test('Gameboard should have a list of ships', () => {
+		let ship = new Ship(Coordinates);
+		ship.type = 'Carrier';
+
+		let coordinates = new Coordinates(0,0);
+		let direction = 'horizontal';
+
+		let anotherShip = new Ship(Coordinates);
+		ship.type = 'Battleship';
+
+		let anotherCoordinates = new Coordinates(5,5);
+		let anotherDirection = 'vertical';
+
+		board.placeShip(ship, coordinates, direction);
+		board.placeShip(anotherShip, anotherCoordinates, anotherDirection);
+
+		expect(board.getShips().every(ship => ship instanceof Ship)).toEqual(true);
+		expect(board.getShips().includes(ship)).toEqual(true);
+		expect(board.getShips().includes(anotherShip)).toEqual(true);
+	});
+
+	test('Ship cannot be placed beyond the boards limits', () => {
+		let ship = new Ship(Coordinates);
+		ship.type = 'Carrier';
+
+		let coordinates = new Coordinates(1337, 9001);
+		let direction = 'horizontal';
+
+		expect(() => board.placeShip(ship, coordinates, direction)).toThrow();
+	});
+
+	test('A ship cannot be placed to extend beyond the boards limits', () => {
+		let ship = new Ship(Coordinates);
+		ship.type = 'Carrier';
+
+		let coordinates = new Coordinates(8, 8);
+		let direction = 'vertical';
+
+		expect(() => board.placeShip(ship, coordinates, direction)).toThrow();
+	});
+
+	test('A ship can be placed and extend within the boards limits', () => {
+		let ship = new Ship(Coordinates);
+		ship.type = 'Carrier';
+
+		let coordinates = new Coordinates(1, 1);
+		let direction = 'horizontal';
+		
+		expect(() => board.placeShip(ship, coordinates, direction)).not.toThrow();
+		expect(board.getShips().length).toEqual(1);
+	});
+
+	test('Ship cannot overlap with placement of another ship', () => {
+		let ship_1 = new Ship(Coordinates);
+		ship_1.type = 'Carrier';
+
+		let coordinates_1 = new Coordinates(3, 3);
+		let direction_1 = 'horizontal';
+
+		let ship_2 = new Ship(Coordinates);
+		ship_2.type = 'Carrier';
+		
+		let coordinates_2 = new Coordinates(4, 1);
+		let direction_2 = 'vertical';
+
+		expect(() => board.placeShip(ship_1, coordinates_1, direction_1)).not.toThrow();
+		expect(board.getShips().length).toEqual(1);
+
+		expect(() => board.placeShip(ship_2, coordinates_2, direction_2)).toThrow();
 	});
 });
 
-describe('Check game board grid', () => {
-	test('Gameboard grid should be size 10x10', () => {
-		expect(board.getGrid().length).toEqual(100);
-	});
+describe('Gameboard receive attack', () => {
+	test.todo('Gameboard will have a list of previous attacks');
 
-	test('Grid coordinates x has a minimum value of 0', () => {
-		const coordinateMinValue = (coordinate) => coordinate.x >= 0;
+	test.todo('Gameboard cannot receive attack where previous attack has occured');
 
-		expect(board.getGrid().every(coordinateMinValue)).toEqual(true);
-	});
+	test.todo('Gameboard cannot receive attack out of board limits');
 
-	test('Grid coordinates y has a minimum value of 0', () => {
-		const coordinateMinValue = (coordinate) => coordinate.y >= 0;
-
-		expect(board.getGrid().every(coordinateMinValue)).toEqual(true);
-	});
-
-	test('Grid coordinates x has a maximum value of 9', () => {
-		const coordinateMaxValue = (coordinate) => coordinate.x <= 9;
-
-		expect(board.getGrid().every(coordinateMaxValue)).toEqual(true);
-	});
-
-	test('Grid coordinates y has a maximum value of 9', () => {
-		const coordinateMaxValue = (coordinate) => coordinate.y <= 9;
-
-		expect(board.getGrid().every(coordinateMaxValue)).toEqual(true);
-	});
+	test.todo('A ship will record a hit when receiving attack is on its coordinates');
 });
-
