b6a93e2db53f953e78ee7f4cbfd7e74854310ae2
Kenya Chan <kenya.chan@gmail.com>
2023-07-11 17:00:24 +1000

    Add position class

---
+++ src/position.js
@@ -0,0 +1,65 @@
+class Coordinates {
+	#x;
+	#y;
+
+	constructor(x, y) {
+		this.#x = x;
+		this.#y = y;
+	}
+
+	get x() {
+		return this.#x;
+	}
+
+	set x(newX) {
+		this.#x = newX;
+	}
+
+	get y() {
+		return this.#y;
+	}
+
+	set y(newY) {
+		this.#y = newY;
+	}
+
+	equals(coordinates) {
+		if(!(coordinates instanceof Coordinates))
+			throw new Error(`"${coordinates}" is not an instance of Coordinates.`);
+
+		return (coordinates.x === this.#x) && (coordinates.y === this.#y) ?
+			true : false;
+	}
+
+	toString() {
+		return `(${this.#x},${this.#y})`;
+	}
+
+}
+
+class Position {
+	#coordinates = [];
+
+	constructor(initialCoordinates, length, direction) {
+		for (let i = 0; i < length; i++) {
+			let coordinates;
+
+			if (direction === 'horizontal')
+				coordinates = new Coordinates(initialCoordinates.x + i, initialCoordinates.y);
+
+			if (direction === 'vertical')
+				coordinates = new Coordinates(initialCoordinates.x, initialCoordinates.y + i);
+
+			this.#coordinates.push(coordinates);
+		}
+	}
+
+	getCoordinates() {
+		return this.#coordinates;
+	}
+}
+
+module.exports = {
+	Coordinates,
+	Position
+}
---
+++ src/tests/position.test.js
@@ -0,0 +1,70 @@
+const { Position, Coordinates } = require('../position');
+
+describe('Coordinates Testing', () => {
+	let coordinates;
+	let randomX;
+	let randomY;
+
+	beforeEach(() => {
+		randomX = Math.floor(Math.random() * 9);
+		randomY = Math.floor(Math.random() * 9);
+
+		coordinates = new Coordinates(randomX, randomY);
+	});
+
+	describe('Check coordinates are created properly', () => {
+		test('x coordinate', () => {
+			expect(coordinates.x).toEqual(randomX);
+		});
+		
+		test('y coordinate', () => {
+			expect(coordinates.y).toEqual(randomY);
+		});
+	});
+
+	describe('Check equality', () => {
+		test('True when coordinates are equal to itself', () => {
+			expect(coordinates.equals(coordinates)).toBeTruthy();
+		});
+
+		test('True when coordinates are equal to another set of coordinates with same values', () => {
+			expect(coordinates.equals(new Coordinates(randomX, randomY))).toBeTruthy();
+		});
+
+		test('Throw an error if comparing with something that is not Coordinates', () => {
+			expect(() => coordinates.equals(1)).toThrow();
+		});
+	});
+
+	describe('Test toString', () => {
+		test('Returns the coordintes as string', () => {
+			expect(coordinates.toString()).toEqual(`(${randomX},${randomY})`);
+		});
+	});
+
+	describe('Updating coordinates', () => {
+		test('X coordinate is updated', () => {
+			expect(coordinates.x).toEqual(randomX);
+			
+			coordinates.x = randomX + 1;
+
+			expect(coordinates.x).toEqual(randomX + 1);
+		});
+
+		test('Y coordinate is updated', () => {
+			expect(coordinates.y).toEqual(randomY);
+
+			coordinates.y = randomY + 1;
+
+			expect(coordinates.y).toEqual(randomY + 1);
+		});
+	});
+});
+
+describe('Position Testing', () => {
+	test('Position will have same number of coordinates as given length', () => {
+		let position = new Position(new Coordinates(0,0), 5, 'horizontal');
+
+		expect(position.getCoordinates()).toHaveLength(5);
+	});
+});
