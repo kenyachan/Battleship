f7a67fd7f0cdf0edbb22a31bdc207c5979e03aba
Kenya Chan <kenya.chan@gmail.com>
2023-08-22 16:39:48 +1000

    WIP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world!');
+import './css/style.css';
+import './img/battleship.jpeg';
+
+const ScreenController = require('./screenController');
+const Battleship = require('./battleship');
+
+let app = new Battleship();
+let sc = new ScreenController(document.querySelector('body'), app.player, app.computerPlayer);
+
+//console.log(app.player.getBoard());
+
+sc.updateBoard(sc.friendlyBoard, app.player.getBoard());
