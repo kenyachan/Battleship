f40353aa015f0cca121747bd64c11cc096670142
Kenya Chan <kenya.chan@gmail.com>
2023-08-22 16:08:55 +1000

    Make x and y coordinates public properties

--- src/coordinates.js
+++ src/coordinates.js
@@ -1,38 +1,19 @@
 class Coordinates {
-	#x;
-	#y;
-
 	constructor(x, y) {
-		this.#x = x;
-		this.#y = y;
-	}
-
-	get x() {
-		return this.#x;
-	}
-
-	set x(newX) {
-		this.#x = newX;
-	}
-
-	get y() {
-		return this.#y;
-	}
-
-	set y(newY) {
-		this.#y = newY;
+		this.x = x;
+		this.y = y;
 	}
 
 	equals(coordinates) {
 		if(!(coordinates instanceof Coordinates))
 			throw new Error(`"${coordinates}" is not an instance of Coordinates.`);
 
-		return (coordinates.x === this.#x) && (coordinates.y === this.#y) ?
+		return (coordinates.x === this.x) && (coordinates.y === this.y) ?
 			true : false;
 	}
 
 	toString() {
-		return `(${this.#x},${this.#y})`;
+		return `(${this.x},${this.y})`;
 	}
 
 }
