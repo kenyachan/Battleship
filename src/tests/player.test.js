87f927cea1807c2f640afdce06362ed78b38680e
Kenya Chan <kenya.chan@gmail.com>
2023-10-31 00:49:32 +1100

    Refactor player test

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -3,74 +3,58 @@ const Gameboard = require('../gameboard');
 
 jest.mock('../gameboard');
 
-describe('Test constructor', () => {	
-	test('Player will be created with a blank gameboard', () => {
-		let friendlyWaters = new Gameboard();
-		let player = new Player(friendlyWaters);
+let player;
+let opponentBoard;
+let targetCoordinates;
+
+beforeEach(() => {
+	player = new Player();
+	
+	opponentBoard = {
+		receiveAttack: jest.fn(coordinates => {
+			return {
+				shotReceived: true
+			};
+		})
+	};
+
+	targetCoordinates = 11;
+});
+
+describe("Creating a new player requires a gameboard", () => {
+	test("Create a new player with a gameboard", () => {
+		let board = new Gameboard();
+		let player = new Player(board);
 
 		expect(player.board instanceof Gameboard).toEqual(true);
-		expect(player.board).toBe(friendlyWaters);
+		expect(player.board).toBe(board);
 	});
 });
 
-describe('Test shoot', () => {
-	let player;
-	let opponentBoard;
-	let targetCoordinates;
-
-	beforeEach(() => {
-		player = new Player();
-		
-		opponentBoard = {
-			receiveAttack: jest.fn(coordinates => {
-				return {
-					shotReceived: true
-				};
-			})
-		};
-
-		targetCoordinates = 11;
-	});
-
-	test('Shoot will call shotReceived on the opponents board with the given coordinates', () => {
+describe("Shoot will attack the given board with given coordinates", () => {
+	test("Shoot will call the receiveAttack function of the given board with targetCoordinates", () => {
 		player.shoot(opponentBoard, targetCoordinates);
 
 		expect(opponentBoard.receiveAttack).toHaveBeenCalledWith(targetCoordinates);
 	});
 
-	test('Shooting the opponents board will return the square being shot', () => {
+	test("Shooting the opponents board will return the square being shot", () => {
 		let targetSquare = player.shoot(opponentBoard, targetCoordinates);
 
 		expect(typeof targetSquare).toBe('object');
 	});
+});
 
-	test('Shooting the same coordinates twice will throw an error', ()=> {
+describe("A player cannot shoot the same coordinates more than once", () => {
+	test("Shooting the same coordinates twice will throw an error", ()=> {
 		let firstShot = player.shoot(opponentBoard, targetCoordinates);
 		
 		expect(() => player.shoot(opponentBoard, targetCoordinates)).toThrow();
 	});
 });
 
-describe('Test shotHistory', () => {
-	let player;
-	let opponentBoard;
-	let targetCoordinates;
-
-	beforeEach(() => {
-		player = new Player();
-		
-		opponentBoard = {
-			receiveAttack: jest.fn(coordinates => {
-				return {
-					shotReceived: true
-				};
-			})
-		};
-
-		targetCoordinates = 11;
-	});
-
-	test('Shot coordinates will be recorded in the shotHistory', () => {
+describe("A player will keep track of the shots taken", () => {
+	test("targetCoordinates will be recorded in the player's shotHistory", () => {
 		player.shoot(opponentBoard, targetCoordinates);
 
 		expect(player.shotHistory.includes(targetCoordinates)).toBe(true);
